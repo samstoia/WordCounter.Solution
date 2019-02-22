fcebb0073f9872248816b23b0e455cc850c5b7bf
samstoia <samstoia@gmail.com>
2019-02-22 09:35:59 -0800

    start building controllers with info from last week's project

---
+++ WordCounter/Controllers/WordCounterController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+
+namespace WordCounter.Controllers
+{
+  public class WordCounterController : Controller
+  {
+    [HttpGet("/wordcounter")]
+    public ActionResult Index()
+    {
+      RepeatCounter newCounter = new RepeatCounter(word, sentence);
+      return View(newCounter);
+    }
+
+    [HttpGet("/wordcounter/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpPost("/wordcounter")]
+    public ActionResult Create(string word, string sentence, int count)
+    {
+      RepeatCounter anotherCounter = new RepeatCounter(word, sentence, count);
+      return View("Index", anotherCounter);
+    }
+  }
+}
