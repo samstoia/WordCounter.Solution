7f7ff677b5ad7b09987dd340c22aab93f0cf7223
samstoia <samstoia@gmail.com>
2019-02-22 08:54:50 -0800

    set up project folders, populate csproj, program, and startup files

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,35 +1,20 @@
-using System;
-using System.Collections.Generic;
-using WordCounter.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WordCounter
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to the word counter. Enter a sentence you would like to analyze.");
-      string sentence = Console.ReadLine();
-      Console.WriteLine("Now enter a word, and we will tell you how many times that word appeared in that senctence");
-      string word = Console.ReadLine();
-      if (word.Length < 1 || sentence.Length < 1)
-      {
-        Console.WriteLine("Sorry, something went wrong.  Check your inputs and try again");
-        Main();
-      }
-      RepeatCounter newCounter = new RepeatCounter(word, sentence);
-      Console.WriteLine("This word appears " + newCounter.GetCount() + " time(s) in that sentence.");
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      Console.WriteLine("Would you like to analyze another sentence? [Y] for yes, [N] for no.");
-      string answer = Console.ReadLine().ToUpper();
-      if(answer == "Y")
-      {
-        Main();
-      }
-      else if(answer == "N")
-      {
-        Environment.Exit(0);
-      }
+      host.Run();
     }
   }
 }
